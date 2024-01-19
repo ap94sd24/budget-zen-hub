1c32eb567161e69e052bf87287a2b2296826ba3e
Adam Pan <adampan94@gmail.com>
2024-01-18 16:34:07 -0800

    Save user auth progress

---
+++ client/src/stores/user.store.ts
@@ -0,0 +1,65 @@
+import { defineStore } from 'pinia'
+
+export const useUserStore = defineStore({
+  id: 'user',
+  state: (): any => ({
+    user: {
+      isAuthenticated: false,
+      id: null,
+      name: null,
+      email: null,
+      access: null,
+      refresh: null
+    }
+  }),
+  actions: {
+    initStore() {
+      if (localStorage.getItem('user.access')) {
+        this.user.access = localStorage.getItem('user.access')
+        this.user.refresh = localStorage.getItem('user.refresh')
+        this.user.id = localStorage.getItem('user.id')
+        this.user.name = localStorage.getItem('user.name')
+        this.user.email = localStorage.getItem('user.email')
+        this.user.isAuthenticated = true
+
+        this.refreshToken()
+        console.log('Init user -> ' + this.user)
+      }
+    },
+    setToken(data: any) {
+      this.user.access = data.access
+      this.user.refresh = data.refresh
+      this.user.isAuthenticated = true
+
+      localStorage.setItem('user.access', data.access)
+      localStorage.setItem('user.refresh', data.refresh)
+    },
+
+    removeToken() {
+      this.user.refresh = null
+      this.user.access = null
+      this.user.isAuthenticated = false
+      this.user.id = false
+      this.user.name = false
+      this.user.email = false
+
+      localStorage.setItem('user.access', '')
+      localStorage.setItem('user.refresh', '')
+      localStorage.setItem('user.id', '')
+      localStorage.setItem('user.name', '')
+      localStorage.setItem('user.email', '')
+    },
+
+    setUserInfo(user: any) {
+      this.user.id = user.id
+      this.user.name = user.name
+      this.user.email = user.email
+
+      localStorage.setItem('user.id', this.user.id)
+      localStorage.setItem('user.name', this.user.name)
+      localStorage.setItem('user.email', this.user.email)
+
+      console.log('User -> ' + this.user)
+    }
+  }
+})
