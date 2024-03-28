4bd29542c98303ef2f750cf206b00248e64091de
Adam Pan <adampan94@gmail.com>
2024-03-28 16:56:06 -0700

    Setup backend only app for vercel

--- client/src/api/notification.ts
+++
@@ -1,7 +0,0 @@
-import http from '@/utils/https';
-
-const rootUrl = '/notifications';
-
-export const getNotifications = async () => await http.get(`${rootUrl}/`);
-
-export const setReadNotification = async (id: string) => await http.post(`${rootUrl}/read/${id}/`);
--- client/src/api/search.ts
+++
@@ -1,3 +0,0 @@
-import http from '@/utils/https';
-
-export const searchQuery = async (data: any) => await http.post(`/search/`, JSON.stringify(data));
